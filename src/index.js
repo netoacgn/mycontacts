165f490cc1ad29493dd7a1776d78b5dd1f7e8c8e
Antonio <acgomesn@gmail.com>
2025-01-21 23:14:33 -0300

    eslint config and editorconfig

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => { 
+app.get('/', (request, response) => {
   response.send('Hello, world!');
+});
 
-})
-
-app.listen(3000, () => console.log('Server started at http://localhost:3000'))
+app.listen(3000, () => console.log('Server started at http://localhost:3000'));
