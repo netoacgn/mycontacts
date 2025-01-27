4f9b5bff611120e40951f7e8377ce2332d7fac34
Antonio <acgomesn@gmail.com>
2025-01-27 20:12:57 -0300

    creating store contact route

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,19 @@
 module.exports = {
-  "env": {
-    "commonjs": true,
-    "es2021": true,
-    "node": true
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
   },
-  "extends": [
-    "airbnb-base"
+  extends: [
+    'airbnb-base',
   ],
-  "parserOptions": {
-    "ecmaVersion": 15
+  parserOptions: {
+    ecmaVersion: 15,
   },
-  "rules": {
-    "class-methods-use-this": "off",
-    "no-promise-executor-return": "off",
-    "consistent-return": "off"
-  }
+  rules: {
+    'class-methods-use-this': 'off',
+    'no-promise-executor-return': 'off',
+    'consistent-return': 'off',
+    camelcase: 'off',
+  },
 };
