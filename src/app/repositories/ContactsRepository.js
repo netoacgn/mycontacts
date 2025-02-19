79bdf98130dade7c0c36d366b182db6bd562b953
Antonio <acgomesn@gmail.com>
2025-02-18 21:24:11 -0300

    refactoring delete method

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,22 +1,4 @@
-const { v4 } = require('uuid');
 const db = require('../database');
-
-let contacts = [
-  {
-    id: v4(),
-    name: 'Antonio',
-    email: 'antonio@gmail.com',
-    phone: '31998291355',
-    category_id: v4(),
-  },
-  {
-    id: v4(),
-    name: 'Robson',
-    email: 'robson@gmail.com',
-    phone: '31998291355',
-    category_id: v4(),
-  },
-];
 
 class ContactsRepository {
   async findAll(orderBy = 'ASC') {
@@ -62,11 +44,13 @@ class ContactsRepository {
     return row;
   }
 
-  delete(id) {
-    return new Promise((resolve) => {
-      contacts = contacts.filter((contact) => contact.id !== id);
-      resolve();
-    });
+  async delete(id) {
+    const deleteOp = await db.query(`
+      DELETE FROM contacts
+      WHERE id = $1
+      `, [id]);
+
+    return deleteOp;
   }
 }
 
